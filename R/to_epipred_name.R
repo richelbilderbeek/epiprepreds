2f1bf647204d52fd18d5b34e6c7277c13c8028e4
richelbilderbeek <richel@richelbilderbeek.nl>
2020-08-19 10:43:01 +0200

    Fix build

--- R/to_epipred_name.R
+++ R/to_epipred_name.R
@@ -2,6 +2,7 @@
 #'
 #' Convert a formally named MHC-I haplotype
 #' to its EpitopePrediction counterpart.
+#' @inheritParams default_params_doc
 #' @export
 to_epipred_name <- function(haplotype) {
   stringr::str_replace_all(
