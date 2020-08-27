13bf593277b41712e050f9ffec06a17d61b2794a
richelbilderbeek <richel@richelbilderbeek.nl>
2020-08-27 12:23:16 +0200

    Explain reason behind bug at https://github.com/richelbilderbeek/bbbq_article/issues/56

--- tests/testthat/test-predict_ic50s.R
+++ tests/testthat/test-predict_ic50s.R
@@ -19,51 +19,19 @@ test_that("epiprepreds", {
 
 test_that("EpitopePrediction bug", {
 
-  skip("WIP")
   # Error at private GitHub repo:
   # https://github.com/richelbilderbeek/bbbq_article/issues/56
   protein_sequence <- "MAAMAVALRGLGGRFRWRTQAVAGGVRGAARGAAAGQRDYDLLVVGGGSGGLACAKEAAQLGRKVAVVDYVEPSPQGTRWGLGGTCVNVGCIPKKLMHQAALLGGLIQDAPNYGWEVAQPVPHDWRKMAEAVQNHVKSLNWGHRVQLQDRKVKYFNIKASFVDEHTVCGVAKGGKEILLSADHIIIATGGRPRYPTHIEGALEYGITSDDIFWLKESPGKTLVVGASYVALECAGFLTGIGLDTTIMMRSIPLRGFDQQMSSMVIEHMASHGTRFLRGCAPSRVRRLPDGQLQVTWEDSTTGKEDTGTFDTVLWAIGRVPDTRSLNLEKAGVDTSPDTQKILVDSREATSVPHIYAIGDVVEGRPELTPIAIMAGRLLVQRLFGGSSDLMDYDNVPTTVFTPLEYGCVGLSEEEAVARHGQEHVEVYHAHYKPLEFTVAGRDASQCYVKMVCLREPPQLVLGLHFLGPNAGEVTQGFALGIKCGASYAQVMRTVGIHPTCSEEVVKLRISKRSGLDPTVTGCUG" # nolint indeed long
   haplotype_name <- "HLA-A*02:01"
   peptide_length <- 9
 
-  # Fails for unknown reason
-  expect_silent(
+  # Failed for unknown reason, reason is now clear
+  expect_error(
     predict_ic50s(
       protein_sequence = protein_sequence,
       peptide_length = peptide_length,
       haplotype_name = haplotype_name
-    )
+    ),
+    "'protein_sequence' contains invalid amino acids"
   )
-  epiprepreds::check_haplotype_name(haplotype_name)
-  ep_haplotype_name <- to_ep_haplotype_name(haplotype_name)
-  epiprepreds::check_ep_haplotype_name(ep_haplotype_name)
-
-  peptides <- epiprepreds::shred_protein(
-    protein_sequence = protein_sequence,
-    peptide_length = peptide_length
-  )
-  peptides[514]
-  peptides[515]
-  peptides[516]
-  Peptides::aaList()
-  # Works
-  EpitopePrediction::smm(
-    x = peptides[1:514],
-    mhc = ep_haplotype_name
-  )
-  EpitopePrediction::smm(
-    x = peptides[515],
-    mhc = ep_haplotype_name
-  )
-  EpitopePrediction::smm(
-    x = peptides[516],
-    mhc = ep_haplotype_name
-  )
-  testthat::expect_equal(
-    nrow(ic50s),
-    nchar(protein_sequence) - peptide_length + 1
-  )
-
-
-
 })
